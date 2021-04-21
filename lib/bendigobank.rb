f5c0ed315213ef59bb2edb2c4fca001978623880
Iury1978 <advent777@yandex.ru>
2021-04-21 14:42:25 +0300

    created a method goto_bank_page

--- lib/bendigobank.rb
+++ lib/bendigobank.rb
@@ -4,5 +4,20 @@ require 'nokogiri'
 
 class BendigoBank
 
-	
+  def initialize
+    @browser = Watir::Browser.new :chrome
+  end
+
+  def start
+    goto_bank_page
+  end
+
+  def goto_bank_page
+    @browser.goto('https://demo.bendigobank.com.au/banking/sign_in')
+    @browser.window.maximize
+    @browser.button(value: 'personal').wait_until(&:present?).click
+    sleep 2
+  end
 end
+
+BendigoBank.new.start
