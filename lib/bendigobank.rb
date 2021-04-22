c41616fd1a47ecd99fb25fc972bd2205765ea92e
Iury1978 <advent777@yandex.ru>
2021-04-22 10:08:35 +0300

    created a get_accounts_id

--- lib/bendigobank.rb
+++ lib/bendigobank.rb
@@ -10,14 +10,33 @@ class BendigoBank
 
   def start
     goto_bank_page
+    get_accounts_id
   end
 
   def goto_bank_page
     @browser.goto('https://demo.bendigobank.com.au/banking/sign_in')
     @browser.window.maximize
     @browser.button(value: 'personal').wait_until(&:present?).click
-    sleep 2
   end
+
+  def get_accounts_id
+    @browser.link(text: 'Accounts').wait_until(&:present?).click
+    # with Nokogiri
+    html =  @browser.ol(class: "grouped-list__group__items").html
+    account_ids_information = Nokogiri::HTML.parse(html)
+    account_ids = account_ids_information.css("[data-semantic='account-number-value']").map do |acc_id|
+      acc_id.text.delete(' ')
+      end
+    # with Watir only
+    # account_ids_watir = @browser.lis(data_semantic: "account-item").map do |li|
+    #   li.attributes[:data_semantic_account_number]
+    #   end
+    # account_ids_watir.pop
+  end
+
+  
+
+
 end
 
 BendigoBank.new.start
