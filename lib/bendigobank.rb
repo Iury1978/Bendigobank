ec401ef9447a279493545ea23c39f71c3495a779
Iury1978 <advent777@yandex.ru>
2021-04-22 12:36:06 +0300

    created parse_accounts and parse_account methods

--- lib/bendigobank.rb
+++ lib/bendigobank.rb
@@ -6,11 +6,12 @@ class BendigoBank
 
   def initialize
     @browser = Watir::Browser.new :chrome
+    @accounts = []
   end
 
   def start
     goto_bank_page
-    get_accounts_id
+    parse_accounts
   end
 
   def goto_bank_page
@@ -34,9 +35,34 @@ class BendigoBank
     # account_ids_watir.pop
   end
 
-  
+  def parse_accounts
+    get_accounts_id.map do |acc_id|
+      @browser.li(data_semantic_account_number: "#{acc_id}").wait_until(&:present?).click
+      html = @browser.div(data_semantic: "account").html
+      account_info = Nokogiri::HTML.parse(html)
 
+      @accounts << parse_account(account_info, acc_id)
+      end
+  end
+  def parse_account(account_info, acc_id)
+  	name = account_info.css("[data-semantic='account-name']").text
+  	available_balance = account_info.css("[data-semantic = 'header-available-balance-amount']").text
+  	# available_balance received data format "$99,999.00"
+  	current_balance = account_info.css("[data-semantic = 'header-current-balance-amount']").text
+  	# current_balance received data format "Minus − $264,321.80" or "$99,999.00"
+  	parsing_currency_and_available_balance(available_balance)
+  	parsing_current_balance(current_balance)
+  	p available_balance
+  	p current_balance
+  end
 
+  def parsing_currency_and_available_balance(available_balance)
+
+  end
+
+  def parsing_current_balance(current_balance)
+  	
+  end
 end
 
 BendigoBank.new.start
