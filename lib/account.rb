0037f09083ae6fbc3b4e4df2a9d9963e2f8ff611
Iury1978 <advent777@yandex.ru>
2021-04-22 11:09:15 +0300

    created account.rb

---
+++ lib/account.rb
@@ -0,0 +1,25 @@
+class Account
+
+attr_accessor :name, :id, :currency, :available_balance, :current_balance, :transactions
+
+  def initialize(parameters)
+    @name = parameters[:name]
+    @id = parameters[:id]
+    @currency = parameters[:currency]
+    @available_balance = parameters[:available_balance]
+    @current_balance = parameters[:current_balance]
+    @transactions = []
+  end
+
+  def to_json(*a)
+  	{
+			name: @name,
+      id: @id,
+			currency: @currency,
+			available_balance: @available_balance,
+      current_balance: @current_balance,
+			transactions: @transactions
+  	}.to_json(*a)
+  end
+  
+end
